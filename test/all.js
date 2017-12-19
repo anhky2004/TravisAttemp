32738cd3cfd0789df1829174783b4d7b1c87496c
anhky2004 <anhky2004@gmail.com>
2017-12-19 16:21:30 +0700

    Update all.js

--- test/all.js
+++ test/all.js
@@ -27,7 +27,7 @@ describe('All', () => {
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('array');
-                    res.body.length.should.be.eql(24); // fixme :)
+                    res.body.length.should.be.eql(18); // fixme :)
                     done();
                 });
         });
